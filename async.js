523e6582365a88e294ce4a2b700704a3614d13fb
litao <litaowork@aliyun.com>
2017-01-07 11:28:24 +0800

    使用 promise 实现 -2

--- async.js
+++ async.js
@@ -55,29 +55,51 @@ const noteAry = [{
 }]
 
 // 先使用 promise 
-noteAry.forEach(note => {
-	if(!existsSync(note.notebook)) {
-		console.log(`笔记本${note.notebook}不存在，先创建笔记本`)
-		// 如果笔记本不存在
-		createNotebook(note.notebook)
-			.then(res => {
-				console.log(`笔记本${note.notebook}创建成功`)
-				return createNote(note)
-			})
-			.then(res => {
-				console.log(`笔记${note.title}创建成功`)
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	} else {
-		console.log(`笔记本${note.notebook}已经存在，直接创建笔记`)
-		createNote(note)
-			.then(res => {
-				console.log(`笔记${note.title}创建成功`)
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	}
+function realCreateNote(note) {
+	return new Promise((resolve, reject) => {
+		if(!existsSync(note.notebook)) {
+			console.log(`笔记本${note.notebook}不存在，先创建笔记本`)
+			// 如果笔记本不存在
+			createNotebook(note.notebook)
+				.then(res => {
+					console.log(`笔记本${note.notebook}创建成功`)
+					return createNote(note)
+				})
+				.then(res => {
+					console.log(`笔记${note.title}创建成功`)
+					resolve(note.title)
+				})
+				.catch(err => {
+					console.log(err)
+					reject(err)
+				})
+		} else {
+			console.log(`笔记本${note.notebook}已经存在，直接创建笔记`)
+			createNote(note)
+				.then(res => {
+					console.log(`笔记${note.title}创建成功`)
+					resolve(note.title)
+				})
+				.catch(err => {
+					console.log(err)
+					reject(err)
+				})
+		}
+	})
+}
+
+function main() {
+	// 临时变量
+	let promise = Promise.resolve()
+	noteAry.forEach(note => {
+		promise = promise.then(() => realCreateNote(note))
+	})
+
+	return promise
+}
+
+main().then(() => {
+	console.log('笔记均创建完成')
+}).catch(err => {
+	console.error(err)
 })
